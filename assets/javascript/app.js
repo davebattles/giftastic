621ee78c213690f10a88d1ca3d275c94b3ca482b
davebattles <davidoflores@gmail.com>
2018-04-30 21:19:48 -0500

    broken img src

---
+++ assets/javascript/app.js
@@ -0,0 +1,159 @@
+$(document).ready(function() {
+
+
+
+
+  var gifArray = ["chicken", "taylor swift", "Kpop", "dance off","rainforest", "space", "captain planet"];
+  var gifBox = $(".buttonSpace");
+  var content = $(".gif-view");
+  var searchBox = $("#searchBox");
+  var addNewGifBtn = $("#add-new-gif-btn");
+
+
+
+
+  /***
+ *    █████████╗   █████╗   ██╗██████╗
+ *    ██╔════██║   ██████╗  ████╔════╝
+ *    █████╗ ██║   ████╔██╗ ████║     
+ *    ██╔══╝ ██║   ████║╚██╗████║     
+ *    ██║    ╚██████╔██║ ╚████╚██████╗
+ *    ╚═╝     ╚═════╝╚═╝  ╚═══╝╚═════╝
+ *                                    
+ */
+
+//clear gifs 
+function clearGifs(){
+  $(".gif-view").empty();
+}
+function clearBtns(){
+  gifBox.html("");
+}
+  // MAKE GIF BTN
+function propagateButtons() {
+ 
+  for( var i = 0; i < gifArray.length ; i++){
+  var buttons = $("<a>");
+  buttons.addClass("btn waves-effect gifbtn");
+  buttons.text(gifArray[i]);
+  buttons.attr("gif-data", gifArray[i]);
+  gifBox.append(buttons);
+  }
+}
+
+// ADD NEW GIF
+addNewGifBtn.on("click", function(event) {
+if(searchBox.val() === "") {
+  (event).preventDefault();
+} else {
+  event.preventDefault();
+  gifArray.push(searchBox.val().trim());
+  clearBtns();
+  propagateButtons();
+  searchBox.val("");
+}
+});
+
+
+
+
+/***
+ *     █████╗      ██╗ █████╗ ██╗  ██╗
+ *    ██╔══██╗     ██║██╔══██╗╚██╗██╔╝
+ *    ███████║     ██║███████║ ╚███╔╝ 
+ *    ██╔══██║██   ██║██╔══██║ ██╔██╗ 
+ *    ██║  ██║╚█████╔╝██║  ██║██╔╝ ██╗
+ *    ╚═╝  ╚═╝ ╚════╝ ╚═╝  ╚═╝╚═╝  ╚═╝
+ */                                    
+
+
+var clickBtn = $(".gifbtn");
+$(document).on("click", ".gifbtn", renderGifs);
+
+
+function renderGifs() {
+  clearGifs();
+  var gifData = $(this).attr("gif-data");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifData + "&api_key=5lOCeJsh28VZ18MUoQMrmIXxQmHUEdrX&limit=10";
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+  //  waits for json to be grabbed
+    .then(function(response) {
+      var result = response.data;
+      var still = "images.fixed_height_still.url";
+      var animate = "images.fixed_height.url";
+      // var rating = result[i].rating;
+
+      for ( var i = 0 ; i < result.length ; i++) {
+
+        //THIS IS WHERE I MESS WITH GIFS
+
+        var div = $("<div>");
+        var img = $("<img>");
+        img.addClass("thisistheimage");
+        img.attr("src", result[i].still);
+        img.attr("data-still", result[i].still);
+        img.attr("data-animate", result[i].animate);
+        img.attr("data-title", result[i].title);
+        img.attr("data-state", "still");
+
+        var p = $("<p>");
+        p.addClass("rating");
+        // p.text("Rated: " + rating);
+
+
+        var title = $("<p>");
+        title.addClass("gif-title");
+        title.text(result[i].title);
+
+        
+       
+          div.prepend(img);
+          
+          div.prepend(p);
+          div.prepend(title);
+
+          content.prepend(div);
+      
+
+
+
+
+
+
+
+      }
+
+
+
+
+
+    });
+
+
+
+
+}
+
+
+
+/***
+ *    ███████████████╗█████╗██████╗████████╗
+ *    ██╔════╚══██╔══██╔══████╔══██╚══██╔══╝
+ *    ███████╗  ██║  █████████████╔╝  ██║   
+ *    ╚════██║  ██║  ██╔══████╔══██╗  ██║   
+ *    ███████║  ██║  ██║  ████║  ██║  ██║   
+ *    ╚══════╝  ╚═╝  ╚═╝  ╚═╚═╝  ╚═╝  ╚═╝   
+ *                                          
+ */
+propagateButtons();
+
+
+
+
+});
